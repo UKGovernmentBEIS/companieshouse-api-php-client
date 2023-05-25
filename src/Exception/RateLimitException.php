6f0ed9f451582a07e0b8628564c797c4bb52d69a
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2023-05-25 12:37:36 +0100

    Removed duplicate exceptions.

--- src/Exception/RateLimitException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace UKGovernmentBEIS\CompaniesHouse;
-
-use Psr\Http\Message\ResponseInterface;
-
-class RateLimitException extends ApiException {}
--- src/Exception/UnauthorisedException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace UKGovernmentBEIS\CompaniesHouse;
-
-use Psr\Http\Message\ResponseInterface;
-
-class UnauthorisedException extends ApiException {}
